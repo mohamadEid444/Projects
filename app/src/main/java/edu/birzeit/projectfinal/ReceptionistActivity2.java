e563cba6f17272ebed92ee857e40e129eacc9c97
mohamadhajali <mohamad184aliq@gmail.com>
2021-12-31 17:13:41 +0200

    haj

---
+++ app/src/main/java/edu/birzeit/projectfinal/ReceptionistActivity2.java
@@ -0,0 +1,14 @@
+package edu.birzeit.projectfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReceptionistActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receptionist2);
+    }
+}
